7c1498d8bf2d17a3876d839ac756d2c282b145fd
spencercjh <jiahao.cai@coscene.io>
2023-01-14 18:55:30 +0800

    reproduce!!!

--- src/main/java/com/example/LegacyHelloGrpcService.java
+++ src/main/java/com/example/LegacyHelloGrpcService.java
@@ -1,7 +1,9 @@
 package com.example;
 
-import io.grpc.Status;
+import com.google.rpc.Code;
+import com.google.rpc.Status;
 import io.grpc.StatusRuntimeException;
+import io.grpc.protobuf.StatusProto;
 import io.grpc.stub.StreamObserver;
 import io.quarkus.grpc.GrpcService;
 
@@ -9,6 +11,9 @@ import io.quarkus.grpc.GrpcService;
 public class LegacyHelloGrpcService extends LegacyHelloGrpcGrpc.LegacyHelloGrpcImplBase {
   @Override
   public void legacySayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
-    responseObserver.onError(new StatusRuntimeException(Status.INVALID_ARGUMENT));
+    final StatusRuntimeException t =
+        StatusProto.toStatusRuntimeException(
+            Status.newBuilder().setCode(Code.INVALID_ARGUMENT_VALUE).build());
+    responseObserver.onError(t);
   }
 }
